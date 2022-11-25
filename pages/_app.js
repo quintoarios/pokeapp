2561f4a1a8f5c8f557250996d5fe2ffe43c5d9c5
Zaragoza <Jose.Zaragoza@cencosud.com.ar>
2022-11-24 21:40:41 -0300

    en blanco

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,14 @@ import * as React from "react";
 import "../styles/globals.scss";
 
 import { NextUIProvider } from "@nextui-org/react";
-import  darkTheme  from "../themes/darkTheme";
+
 
 
 
 
 function MyApp({ Component, pageProps }) {
   return (
-  <NextUIProvider theme={darkTheme}>
+  <NextUIProvider >
     <Component {...pageProps} />
   </NextUIProvider>
   );
